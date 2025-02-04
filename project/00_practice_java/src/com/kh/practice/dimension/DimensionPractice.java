935c5c042806823406b64c25811b537276283daf
fstwon <fstwon@gmail.com>
2025-02-04 23:44:40 +0900

    2차원 배열 실습문제 (진행중)

--- project/00_practice_java/src/com/kh/practice/dimension/DimensionPractice.java
+++ project/00_practice_java/src/com/kh/practice/dimension/DimensionPractice.java
@@ -1,4 +1,5 @@
 package com.kh.practice.dimension;
+import java.util.Scanner;
 
 public class DimensionPractice {
 
@@ -6,7 +7,119 @@ public class DimensionPractice {
 		// practice1();
 		// practice2();
 		// practice3();
-		practice4();
+		// practice4();
+		// practice5();
+		// practice6();
+		// practice7();
+		practice8();
+	}
+	public static void practice8() {
+		// [출석부]
+		// 1. 강건강 2. 남나나 3. 도대담 4. 류라라 5. 문미미 6. 박보배
+		// 7. 송성실 8. 윤예의 9. 진재주 10. 차천축 11. 피풍표 12. 홍하하
+		// 1차원 배열에 학생 출석부 순 초기화
+		String[] arr = { "강건강", "남나나", "도대담", "류라라", "문미미", "박보배", "송성실", "윤예의", "진재주", "차천축", "피풍표", "홍하하" };
+		String[][] arr2 = new String[3][2];
+		String[][] arr3 = new String[3][2];
+		// 2열 3행으로 2차원 배열 2개 나눠
+		for(int i = 0; i < arr.length; i++) {
+			
+		}
+		// 왼쪽부터 오른쪽, 1행에서 아래 행 순으로 자리 배치 
+		
+		// 데이터 출력
+		
+	}
+	public static void practice7() {
+		Scanner sc = new Scanner(System.in);
+		// 사용자 행 크기 입력
+		System.out.print("행의 크기 : ");
+		int row = sc.nextInt();
+		char[][] chArr = new char[row][];
+		
+		char ch = 'a';
+		// 행 크기 만큼 반복하여 열 크기 입력
+		for(int i = 0; i < row; i++) {
+			System.out.printf("%d행의 열 크기 : ", i);
+			int col = sc.nextInt();
+			
+			// 문자형 가변 배열 선언 및 할당
+			chArr[i] = new char[col];
+			for(int j = 0; j < col; j++) {
+				// 각 인덱스에 'a'부터 총 인덱스의 개수만큼 하나씩 늘려 저장하고 출력 char a + 1
+				chArr[i][j] = ch++;
+			}
+		}
+		
+		
+		// 데이터 출력
+		for(int i = 0; i < chArr.length; i++) {
+			for(int j = 0; j < chArr[i].length; j++) {
+				System.out.print(chArr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void practice6() {
+		// 초기화 배열 출력하기 근데 행과 열을 바꿔서
+		// print 사용, 값 사이 띄어쓰기 추가
+		String[][] strArr = new String[][] {
+			{"이", "까", "왔", "앞", "힘"},
+			{"차", "지", "습", "으", "냅"},
+			{"원", "열", "니", "로", "시"},
+			{"배", "심", "다", "좀", "다"},
+			{"열", "히", "! ", "더", "!! "}
+		};
+		
+		for(int i = 0; i < strArr.length; i++) {
+			for(int j = 0; j < strArr[i].length; j++) {
+				// 한 줄 출력 시 열 고정 행만 변경
+				// j 반복문이 도는 동안 고정 값은 i
+				System.out.print(strArr[j][i] + " ");
+			}
+			System.out.println();
+		}
+	}
+	public static void practice5() {
+		Scanner sc = new Scanner(System.in);
+		int row;
+		int col;
+		char[][] chArr = new char[0][];
+		
+		do {
+			// 2차원 배열의 행과 열의 크기 사용자 입력
+			System.out.print("행 크기 : ");
+			row = sc.nextInt();
+			
+			System.out.print("열 크기 : ");
+			col = sc.nextInt();
+			
+			// 1 ~ 10사이 숫자가 아니면 "반드시 1 ~ 10 사이의 정수를 입력해야 합니다." 출력 후 다시 입력
+			if(1 > row || row > 10 || 1 > col || col > 10) {
+				System.out.println("반드시 1 ~ 10 사이의 정수를 입력해야 합니다.");
+				continue;
+			}
+
+			chArr = new char[row][col];
+			
+			// 크기가 정해진 2차원 배열에 영어 대문자 랜덤 할당, A ~ Z, 65 ~ 90, 25 
+			for(int i = 0; i < row; i++) {
+				for(int j = 0; j < col; j++) {
+					int random = (int)(Math.random() * 26); // 0 ~ 24
+					char a = 'A';
+					chArr[i][j] = (char)(a + random);
+				}
+			}
+		} while(1 > row || row > 10 || 1 > col || col > 10);
+		
+		
+		// 데이터 출력
+		for(int i = 0; i < row; i++) {
+			for(int j = 0; j < col; j++) {
+				System.out.printf("%-3c", chArr[i][j]);
+			}
+			System.out.println();
+		}
 	}
 	public static void practice4() {
 		// 4행 4열 2차원 배열 생성
@@ -14,7 +127,7 @@ public class DimensionPractice {
 		// 0행 0열 ~ 2행 2열, 1 ~ 10 중 임의 정수 할당
 		for(int i = 0; i < arr.length; i++) {
 			for(int j = 0; j < arr[i].length; j++) {
-				if(j < arr[i].length - 1) {
+				if(j < arr[i].length - 1 && i < arr.length) {
 					// 난수 생성
 					int random = (int)(Math.random() * 10 + 1);
 					// 할당
